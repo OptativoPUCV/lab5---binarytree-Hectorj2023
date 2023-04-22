42aa78c272734fee6acab33c07a068be92a8290a
Replit user <>
2023-04-22 15:36:36 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -110,13 +110,13 @@ void removeNode(TreeMap * tree, TreeNode* node) {
         free(node);
     }
 
-   // else {
-   // TreeNode* successor = minimum(node->right);
-    //node->key = successor->key;
-   // node->value = successor->value;
-   // node->balance = successor->balance;
-   // removeNode(tree, successor);
-   // }
+    else {
+    TreeNode* parent = minimum(node->right);
+    node->key = parent->key;
+    node->value = parent->value;
+    node->balance = parent->balance;
+    removeNode(tree, parent);
+   }
 
 }
 
